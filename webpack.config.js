67b6a8adad6f857a35878933685ca401cb92c6a5
Shanaaz Ahamed <shanaaz.19@cse.mrt.ac.lk>
2024-05-26 17:22:48 +0530

    refactor: Remove unused CSS and JavaScript rules in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -18,22 +18,4 @@ module.exports = {
       patterns: [{ from: "static" }],
     }),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
-  },
 };
